7f33e7fb6ba577e983a9516bb0957eb534b2dc5e
Daniel Ecer <de-code@users.noreply.github.com>
2023-02-08 08:38:47 +0000

    add authors to article card

--- sciety_discovery/models/article.py
+++ sciety_discovery/models/article.py
@@ -1,9 +1,10 @@
-from typing import NamedTuple, Optional
+from typing import NamedTuple, Optional, Sequence
 
 
 class ArticleMetaData(NamedTuple):
     article_doi: str
     article_title: str
+    author_name_list: Optional[Sequence[str]] = None
 
 
 class ArticleMention(NamedTuple):
---
+++ tests/unit_tests/providers/crossref_test.py
@@ -0,0 +1,36 @@
+from sciety_discovery.providers.crossref import (
+    get_article_metadata_from_crossref_metadata
+)
+
+
+DOI_1 = '10.1101/doi1'
+
+
+CROSSREF_RESPONSE_MESSAGE_1: dict = {
+    'title': [],
+    'author': []
+}
+
+
+class TestGetArticleMetadataFromCrossrefMetadata:
+    def test_should_extract_single_line_title(self):
+        result = get_article_metadata_from_crossref_metadata(
+            DOI_1,
+            {**CROSSREF_RESPONSE_MESSAGE_1, 'title': ['Title 1']}
+        )
+        assert result.article_title == 'Title 1'
+
+    def test_should_extract_author_names(self):
+        result = get_article_metadata_from_crossref_metadata(
+            DOI_1,
+            {
+                **CROSSREF_RESPONSE_MESSAGE_1,
+                'author': [{
+                    'given': 'John',
+                    'family': 'Smith'
+                }]
+            }
+        )
+        assert result.author_name_list == [
+            'John Smith'
+        ]
