4415fea264fc3b6c48235dfd64fef9abb9893e10
Daniel Ecer <de-code@users.noreply.github.com>
2023-03-31 11:46:32 +0100

    Added group lists to home page

--- sciety_labs/main.py
+++ sciety_labs/main.py
@@ -217,7 +217,7 @@ def create_app():  # pylint: disable=too-many-locals, too-many-statements
 
     @app.get('/', response_class=HTMLResponse)
     async def index(request: Request):
-        list_summary_data_list = list(
+        user_list_summary_data_list = list(
             google_sheet_list_image_provider.iter_list_summary_data_with_list_image_url(
                 lists_model.get_most_active_user_lists(
                     top_n=3,
@@ -225,11 +225,21 @@ def create_app():  # pylint: disable=too-many-locals, too-many-statements
                 )
             )
         )
-        LOGGER.info('list_summary_data_list: %r', list_summary_data_list)
+        LOGGER.info('user_list_summary_data_list: %r', user_list_summary_data_list)
+        group_list_summary_data_list = list(
+            google_sheet_list_image_provider.iter_list_summary_data_with_list_image_url(
+                lists_model.get_most_active_group_lists(
+                    top_n=3,
+                    min_article_count=min_article_count
+                )
+            )
+        )
+        LOGGER.info('group_list_summary_data_list: %r', group_list_summary_data_list)
         return templates.TemplateResponse(
             'pages/index.html', {
                 'request': request,
-                'user_lists': list_summary_data_list
+                'user_lists': user_list_summary_data_list,
+                'group_lists': group_list_summary_data_list
             }
         )
 
