38456414003699e8d041440eed8939e08d2e791d
Daniel Ecer <de-code@users.noreply.github.com>
2023-06-09 10:05:45 +0100

    Add feed image to RSS feed

--- sciety_labs/app/routers/search.py
+++ sciety_labs/app/routers/search.py
@@ -308,7 +308,8 @@ def create_search_router(
                 ),
                 'search_parameters_hash': search_feed_parameters.search_parameters.get_hash(),
                 'page_title': search_feed_parameters.page_title,
-                'page_description': search_feed_parameters.page_description
+                'page_description': search_feed_parameters.page_description,
+                'page_images': search_feed_parameters.feed_images
             },
             media_type=AtomResponse.media_type,
             status_code=search_result_page.status_code
