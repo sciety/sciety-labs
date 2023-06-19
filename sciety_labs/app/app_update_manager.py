dd6cbef57d410fe04e42938ab921f94f6b2253f2
Daniel Ecer <de-code@users.noreply.github.com>
2023-06-19 15:42:45 +0100

    Made background updates more resilient (#115)
    * Made background updates more resilient
    
    * Reverted temporary update interval change

--- sciety_labs/app/app_update_manager.py
+++ sciety_labs/app/app_update_manager.py
@@ -1,5 +1,10 @@
+import logging
+
 from sciety_labs.app.app_providers_and_models import AppProvidersAndModels
 from sciety_labs.utils.threading import UpdateThread
+
+
+LOGGER = logging.getLogger(__name__)
 
 
 class AppUpdateManager:
@@ -16,8 +21,17 @@ class AppUpdateManager:
         self.app_providers_and_models.google_sheet_article_image_provider.refresh()
         self.app_providers_and_models.google_sheet_list_image_provider.refresh()
 
+    def check_or_reload_data_no_fail(self):
+        try:
+            self.check_or_reload_data()
+        except InterruptedError:
+            LOGGER.info('Updates interrupted, app closed?')
+            raise
+        except Exception as exc:  # pylint: disable=broad-except
+            LOGGER.error('Caught error handling update: %s', exc, exc_info=1)
+
     def start(self, update_interval_in_secs: float):
         UpdateThread(
             update_interval_in_secs=update_interval_in_secs,
-            update_fn=self.check_or_reload_data
+            update_fn=self.check_or_reload_data_no_fail
         ).start()
