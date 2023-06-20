bb2812b014c2e2ca224e69dd1645af4f73e62ace
Daniel Ecer <de-code@users.noreply.github.com>
2023-06-20 14:27:59 +0100

    Using pyarrow when loading data from BigQuery

--- sciety_labs/providers/sciety_event.py
+++ sciety_labs/providers/sciety_event.py
@@ -3,7 +3,9 @@ from pathlib import Path
 from time import monotonic
 from typing import Optional, Sequence
 
-from sciety_labs.utils.bigquery import iter_dict_from_bq_query
+import pyarrow
+
+from sciety_labs.utils.bigquery import get_arrow_table_from_bq_query
 from sciety_labs.providers.sql import get_sql_path
 from sciety_labs.utils.cache import DummySingleObjectCache, SingleObjectCache
 
@@ -25,20 +27,24 @@ class ScietyEventProvider:
             query_results_cache = DummySingleObjectCache[Sequence[dict]]()
         self._query_results_cache = query_results_cache
 
-    def _load_query_results_from_bq(self) -> Sequence[dict]:
+    def _load_query_results_from_bq_as_arrow_table(self) -> pyarrow.Table:
         LOGGER.info('Loading query results from BigQuery...')
         start_time = monotonic()
-        result = list(iter_dict_from_bq_query(
+        arrow_table = get_arrow_table_from_bq_query(
             self.gcp_project_name,
             self.get_sciety_events_query
-        ))
+        )
         end_time = monotonic()
         LOGGER.info(
             'Loaded query results from BigQuery, rows=%d, time=%.3f seconds',
-            len(result),
+            len(arrow_table),
             (end_time - start_time)
         )
-        return result
+        return arrow_table
+
+    def _load_query_results_from_bq(self) -> Sequence[dict]:
+        query_results = self._load_query_results_from_bq_as_arrow_table().to_pylist()
+        return query_results
 
     def get_sciety_event_dict_list(self) -> Sequence[dict]:
         return self._query_results_cache.get_or_load(
