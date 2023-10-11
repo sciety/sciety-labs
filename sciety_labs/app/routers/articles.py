21ab4289eaeb8cc905bbe97b5e80c21ad36de4d1
Daniel Ecer <de-code@users.noreply.github.com>
2023-10-11 16:56:10 +0100

    Extend days to 120

--- sciety_labs/app/routers/articles.py
+++ sciety_labs/app/routers/articles.py
@@ -32,7 +32,7 @@ def get_article_recommendation_filter_parameters(
 ) -> ArticleRecommendationFilterParameters:
     return ArticleRecommendationFilterParameters(
         exclude_article_dois={article_doi},
-        from_publication_date=date.today() - timedelta(days=60),
+        from_publication_date=date.today() - timedelta(days=120),
         evaluated_only=True
     )
 
