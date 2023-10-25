d6c3980e28ac4126d650f5176fe8db0e1811c411
Daniel Ecer <de-code@users.noreply.github.com>
2023-10-25 14:59:19 +0100

    OpenSearch: Use `preprints_v2` index (#239)
    * Introduced OPENSEARCH_INDEX_V2_NAME
    
    * Changed field names to match preprints_v2 index
    
    * Introduced OPENSEARCH_INDEX_V2_EMBEDDING_VECTOR_MAPPING_NAME

--- tests/unit_tests/providers/opensearch_test.py
+++ tests/unit_tests/providers/opensearch_test.py
@@ -25,8 +25,10 @@ class TestOpenSearchConnectionConfig:
         env_mock[OpenSearchEnvVariables.OPENSEARCH_PORT] = '1234'
         env_mock[OpenSearchEnvVariables.OPENSEARCH_USERNAME_FILE_PATH] = str(username_file_path)
         env_mock[OpenSearchEnvVariables.OPENSEARCH_PASSWORD_FILE_PATH] = str(password_file_path)
-        env_mock[OpenSearchEnvVariables.OPENSEARCH_INDEX_NAME] = 'index1'
-        env_mock[OpenSearchEnvVariables.OPENSEARCH_EMBEDDING_VECTOR_MAPPING_NAME] = 'vector1'
+        env_mock[OpenSearchEnvVariables.OPENSEARCH_INDEX_V2_NAME] = 'index1'
+        env_mock[OpenSearchEnvVariables.OPENSEARCH_INDEX_V2_EMBEDDING_VECTOR_MAPPING_NAME] = (
+            'vector1'
+        )
         config = OpenSearchConnectionConfig.from_env()
         assert config == OpenSearchConnectionConfig(
             hostname='hostname1',
