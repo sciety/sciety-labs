58945ed7f8c9c949c98223510f724835d9c2d2c5
Daniel Ecer <de-code@users.noreply.github.com>
2024-01-04 18:03:54 +0000

    Fixed exception handling due to required position argument

--- sciety_labs/app/routers/api/app.py
+++ sciety_labs/app/routers/api/app.py
@@ -34,7 +34,10 @@ def create_api_app(
     ))
 
     @app.exception_handler(Exception)
-    async def generic_exception_handler(exception: Exception):
+    async def generic_exception_handler(
+        request: fastapi.Request,  # pylint: disable=unused-argument
+        exception: Exception
+    ):
         LOGGER.warning('Error: %r', exception)
         return fastapi.responses.JSONResponse(
             content={
