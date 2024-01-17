8045347630a37e76ae93023d48c39e931110fb44
Daniel Ecer <de-code@users.noreply.github.com>
2024-01-17 05:02:57 +0000

    Custom validation error page

--- sciety_labs/app/main.py
+++ sciety_labs/app/main.py
@@ -2,6 +2,7 @@ from http.client import HTTPException
 import logging
 
 from fastapi import FastAPI, Request
+from fastapi.exceptions import RequestValidationError
 from fastapi.staticfiles import StaticFiles
 
 from sciety_labs.app.app_providers_and_models import AppProvidersAndModels
@@ -92,6 +93,20 @@ def _create_app():  # pylint: disable=too-many-locals, too-many-statements
                 'exception': exception
             },
             status_code=404
+        )
+
+    @app.exception_handler(RequestValidationError)
+    async def validation_error_exception_handler(request: Request, exception: HTTPException):
+        error_message = 'Something doesn\'t seem right, with the parameters.'
+        return templates.TemplateResponse(
+            request=request,
+            name='errors/error.html',
+            context={
+                'page_title': get_page_title(error_message),
+                'error_message': error_message,
+                'exception': exception
+            },
+            status_code=400
         )
 
     @app.exception_handler(500)
