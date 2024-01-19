5d67c9ffd9a84d283edbcce27e33272006bf25fe
Daniel Ecer <de-code@users.noreply.github.com>
2024-01-19 18:45:38 +0000

    Show error message for ListNotFoundError

--- sciety_labs/app/app_error_handlers.py
+++ sciety_labs/app/app_error_handlers.py
@@ -7,6 +7,7 @@ from fastapi.templating import Jinja2Templates
 from sciety_labs.app.utils.common import (
     get_page_title
 )
+from sciety_labs.models.lists import ListNotFoundError
 
 
 def add_app_error_handlers(
@@ -49,4 +50,24 @@ def add_app_error_handlers(
                 'exception': exception
             },
             status_code=500
+        )
+
+    @app.exception_handler(ListNotFoundError)
+    async def list_not_found_error_exception_handler(
+        request: Request,
+        exception: ListNotFoundError
+    ):
+        list_id = exception.list_id
+        error_message = (
+            'List not found. This could be because the data isn\'t yet available in Sciety Labs.'
+        )
+        return templates.TemplateResponse(
+            request=request,
+            name='errors/error.html',
+            context={
+                'page_title': get_page_title(f'List not found: {list_id}'),
+                'error_message': error_message,
+                'exception': exception
+            },
+            status_code=404
         )
