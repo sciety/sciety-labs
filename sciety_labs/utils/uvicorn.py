7ff5799beee6d84bd0de2072fbc0134689519ab2
Daniel Ecer <de-code@users.noreply.github.com>
2024-01-22 17:25:50 +0000

    Redirect for url encoded double query parameters (#320)
    * Extracted get_redirect_url_for_double_query_string_url_or_none
    
    * Also split on URL encoded question mark
    
    * Reduce logging level of query parameter

--- sciety_labs/utils/uvicorn.py
+++ sciety_labs/utils/uvicorn.py
@@ -1,4 +1,6 @@
 import logging
+import re
+from typing import Optional
 
 from starlette.datastructures import URL
 from starlette.responses import RedirectResponse
@@ -6,6 +8,15 @@ from starlette.types import ASGIApp, Receive, Scope, Send
 
 
 LOGGER = logging.getLogger(__name__)
+
+
+def get_redirect_url_for_double_query_string_url_or_none(url: URL) -> Optional[str]:
+    LOGGER.debug('url.query: %r', url.query)
+    if url.query:
+        first_query_string, *other_query_strings = re.split(r'\?|%3F', url.query, maxsplit=1)
+        if other_query_strings:
+            return str(url.replace(query=first_query_string))
+    return None
 
 
 class RedirectDoubleQueryStringMiddleware:
@@ -18,14 +29,12 @@ class RedirectDoubleQueryStringMiddleware:
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         LOGGER.debug('scope: %r', scope)
         url = URL(scope=scope)
-        if url.query:
-            first_query_string, *other_query_strings = url.query.split('?', maxsplit=1)
-            if other_query_strings:
-                redirect_url = str(url.replace(query=first_query_string))
-                LOGGER.info('Redirecting to (due to double query string): %r', redirect_url)
-                response = RedirectResponse(url=redirect_url, status_code=301)
-                await response(scope, receive, send)
-                return
+        redirect_url = get_redirect_url_for_double_query_string_url_or_none(url)
+        if redirect_url:
+            LOGGER.info('Redirecting to (due to double query string): %r', redirect_url)
+            response = RedirectResponse(url=redirect_url, status_code=301)
+            await response(scope, receive, send)
+            return
         await self.app(scope, receive, send)
 
 
