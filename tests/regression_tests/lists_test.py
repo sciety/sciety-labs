e5d9eeaa7cb17a07b42b793ff786c7b36f8ea207
Daniel Ecer <de-code@users.noreply.github.com>
2024-02-07 14:55:56 +0000

    Test related article RSS

--- tests/regression_tests/lists_test.py
+++ tests/regression_tests/lists_test.py
@@ -61,6 +61,12 @@ class TestListRecommendationsPage:
         )
         response.raise_for_status()
 
+    def test_should_load_user_list_page_rss(self, regression_test_session: Session):
+        response = regression_test_session.get(
+            f'/lists/by-id/{USER_LIST_ID}/article-recommendations/atom.xml'
+        )
+        response.raise_for_status()
+
     def test_should_load_group_list_page(self, regression_test_session: Session):
         response = regression_test_session.get(
             f'/lists/by-id/{GROUP_LIST_ID}/article-recommendations'
@@ -71,5 +77,11 @@ class TestListRecommendationsPage:
         response = regression_test_session.get(
             f'/lists/by-id/{GROUP_LIST_ID}/article-recommendations',
             params={'fragment': True}
+        )
+        response.raise_for_status()
+
+    def test_should_load_group_list_page_rss(self, regression_test_session: Session):
+        response = regression_test_session.get(
+            f'/lists/by-id/{GROUP_LIST_ID}/article-recommendations/atom.xml'
         )
         response.raise_for_status()
