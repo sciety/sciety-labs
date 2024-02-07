d5a4cb3a86e46f86dd4b42c777962c825178f14c
Daniel Ecer <de-code@users.noreply.github.com>
2024-02-07 14:21:15 +0000

    Added list page regression tests

--- tests/regression_tests/lists_test.py
+++ tests/regression_tests/lists_test.py
@@ -2,6 +2,8 @@
 
 from requests import Session
 
+from tests.regression_tests.test_data import GROUP_LIST_ID, USER_LIST_ID
+
 
 class TestListsPage:
     def test_should_load_user_lists_page(self, regression_test_session: Session):
@@ -13,5 +15,19 @@ class TestListsPage:
     def test_should_load_group_lists_page(self, regression_test_session: Session):
         response = regression_test_session.get(
             '/lists/group-lists'
+        )
+        response.raise_for_status()
+
+
+class TestListPage:
+    def test_should_load_user_list_page(self, regression_test_session: Session):
+        response = regression_test_session.get(
+            f'/lists/by-id/{USER_LIST_ID}'
+        )
+        response.raise_for_status()
+
+    def test_should_load_group_list_page(self, regression_test_session: Session):
+        response = regression_test_session.get(
+            f'/lists/by-id/{GROUP_LIST_ID}'
         )
         response.raise_for_status()
