cd059caffd9cf791f3688d0064ea35a7e1646aed
Daniel Ecer <de-code@users.noreply.github.com>
2024-02-07 14:46:35 +0000

    Added list recommendation page regression tests

--- tests/regression_tests/lists_test.py
+++ tests/regression_tests/lists_test.py
@@ -31,3 +31,31 @@ class TestListPage:
             f'/lists/by-id/{GROUP_LIST_ID}'
         )
         response.raise_for_status()
+
+
+class TestListRecommendationsPage:
+    def test_should_load_user_list_page(self, regression_test_session: Session):
+        response = regression_test_session.get(
+            f'/lists/by-id/{USER_LIST_ID}/article-recommendations'
+        )
+        response.raise_for_status()
+
+    def test_should_load_user_list_page_fragment(self, regression_test_session: Session):
+        response = regression_test_session.get(
+            f'/lists/by-id/{USER_LIST_ID}/article-recommendations',
+            params={'fragment': True}
+        )
+        response.raise_for_status()
+
+    def test_should_load_group_list_page(self, regression_test_session: Session):
+        response = regression_test_session.get(
+            f'/lists/by-id/{GROUP_LIST_ID}/article-recommendations'
+        )
+        response.raise_for_status()
+
+    def test_should_load_group_list_page_fragment(self, regression_test_session: Session):
+        response = regression_test_session.get(
+            f'/lists/by-id/{GROUP_LIST_ID}/article-recommendations',
+            params={'fragment': True}
+        )
+        response.raise_for_status()
