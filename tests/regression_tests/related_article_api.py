ad2fc7728a3a40d624211cb135e1f11418e2253f
Daniel Ecer <de-code@users.noreply.github.com>
2024-02-08 14:44:34 +0000

    Made default like s2 API endpoint async (#340)
    * Made async API the default
    
    * Added regression test for the API
    
    * Also check there are some recommended papers in the regression API test

---
+++ tests/regression_tests/related_article_api.py
@@ -0,0 +1,16 @@
+
+
+from requests import Session
+from sciety_labs.app.routers.api.article_recommendation import RecommendationResponseDict
+
+from tests.regression_tests.test_data import DOI_1
+
+
+class TestRelatedArticlesApi:
+    def test_should_load_related_article_results(self, regression_test_session: Session):
+        response = regression_test_session.get(
+            f'/api/like/s2/recommendations/v1/papers/forpaper/DOI:{DOI_1}'
+        )
+        response.raise_for_status()
+        response_json: RecommendationResponseDict = response.json()
+        assert len(response_json['recommendedPapers']) > 0
