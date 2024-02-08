1a6c521d7474fddc2c42a7332955d949c99f8c0e
Daniel Ecer <de-code@users.noreply.github.com>
2024-02-08 11:54:12 +0000

    Reduce duplicate group and user list tests by using list id fixture

--- tests/regression_tests/lists_test.py
+++ tests/regression_tests/lists_test.py
@@ -1,8 +1,17 @@
 
 
+import pytest
 from requests import Session
 
 from tests.regression_tests.test_data import SCIETY_GROUP_LIST_ID_1, SCIETY_USER_LIST_ID_1
+
+
+@pytest.fixture(name='sciety_list_id', params=[
+    SCIETY_GROUP_LIST_ID_1,
+    SCIETY_USER_LIST_ID_1
+])
+def _sciety_list_id(request: pytest.FixtureRequest) -> str:
+    return request.param
 
 
 class TestListOfListPage:
@@ -20,68 +29,57 @@ class TestListOfListPage:
 
 
 class TestListPage:
-    def test_should_load_user_list_page(self, regression_test_session: Session):
+    def test_should_load_list_page(
+        self,
+        regression_test_session: Session,
+        sciety_list_id: str
+    ):
         response = regression_test_session.get(
-            f'/lists/by-id/{SCIETY_USER_LIST_ID_1}'
-        )
-        response.raise_for_status()
-
-    def test_should_load_group_list_page(self, regression_test_session: Session):
-        response = regression_test_session.get(
-            f'/lists/by-id/{SCIETY_GROUP_LIST_ID_1}'
+            f'/lists/by-id/{sciety_list_id}'
         )
         response.raise_for_status()
 
 
 class TestListRssPage:
-    def test_should_load_user_list_rss_page(self, regression_test_session: Session):
+    def test_should_load_list_rss_page(
+        self,
+        regression_test_session: Session,
+        sciety_list_id: str
+    ):
         response = regression_test_session.get(
-            f'/lists/by-id/{SCIETY_USER_LIST_ID_1}/atom.xml'
-        )
-        response.raise_for_status()
-
-    def test_should_load_group_list_rss_page(self, regression_test_session: Session):
-        response = regression_test_session.get(
-            f'/lists/by-id/{SCIETY_GROUP_LIST_ID_1}/atom.xml'
+            f'/lists/by-id/{sciety_list_id}/atom.xml'
         )
         response.raise_for_status()
 
 
 class TestListRecommendationsPage:
-    def test_should_load_user_list_page(self, regression_test_session: Session):
+    def test_should_load_user_list_page(
+        self,
+        regression_test_session: Session,
+        sciety_list_id: str
+    ):
         response = regression_test_session.get(
-            f'/lists/by-id/{SCIETY_USER_LIST_ID_1}/article-recommendations'
+            f'/lists/by-id/{sciety_list_id}/article-recommendations'
         )
         response.raise_for_status()
 
-    def test_should_load_user_list_page_fragment(self, regression_test_session: Session):
+    def test_should_load_list_page_fragment(
+        self,
+        regression_test_session: Session,
+        sciety_list_id: str
+    ):
         response = regression_test_session.get(
-            f'/lists/by-id/{SCIETY_USER_LIST_ID_1}/article-recommendations',
+            f'/lists/by-id/{sciety_list_id}/article-recommendations',
             params={'fragment': True}
         )
         response.raise_for_status()
 
-    def test_should_load_user_list_page_rss(self, regression_test_session: Session):
+    def test_should_load_user_list_page_rss(
+        self,
+        regression_test_session: Session,
+        sciety_list_id: str
+    ):
         response = regression_test_session.get(
-            f'/lists/by-id/{SCIETY_USER_LIST_ID_1}/article-recommendations/atom.xml'
-        )
-        response.raise_for_status()
-
-    def test_should_load_group_list_page(self, regression_test_session: Session):
-        response = regression_test_session.get(
-            f'/lists/by-id/{SCIETY_GROUP_LIST_ID_1}/article-recommendations'
-        )
-        response.raise_for_status()
-
-    def test_should_load_group_list_page_fragment(self, regression_test_session: Session):
-        response = regression_test_session.get(
-            f'/lists/by-id/{SCIETY_GROUP_LIST_ID_1}/article-recommendations',
-            params={'fragment': True}
-        )
-        response.raise_for_status()
-
-    def test_should_load_group_list_page_rss(self, regression_test_session: Session):
-        response = regression_test_session.get(
-            f'/lists/by-id/{SCIETY_GROUP_LIST_ID_1}/article-recommendations/atom.xml'
+            f'/lists/by-id/{sciety_list_id}/article-recommendations/atom.xml'
         )
         response.raise_for_status()
