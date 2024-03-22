0eddb1c1c39d5bbe162ee00b911b865cdd8fe34f
Daniel Ecer <de-code@users.noreply.github.com>
2024-03-22 18:49:21 +0000

    Added combined load test

---
+++ tests/load_tests/all_test.py
@@ -0,0 +1,17 @@
+# pylint: disable=unused-import
+
+from tests.load_tests.homepage_test import (  # noqa
+    ScietyLabsHomepageUser
+)
+
+from tests.load_tests.api_related_article_test import (  # noqa
+    ScietyLabsApiRelatedArticlesUser
+)
+
+from tests.load_tests.list_of_lists_test import (  # noqa
+    ScietyLabsListOfListsUser
+)
+
+from tests.load_tests.list_test import (  # noqa
+    ScietyLabsListUser
+)
