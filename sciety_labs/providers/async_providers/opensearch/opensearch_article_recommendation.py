f06076db9a2ca23ec1d8e646a81423734517e608
Daniel Ecer <de-code@users.noreply.github.com>
2024-03-22 17:22:58 +0000

    Moved opensearch typing to separate module

--- sciety_labs/providers/async_providers/opensearch/opensearch_article_recommendation.py
+++ sciety_labs/providers/async_providers/opensearch/opensearch_article_recommendation.py
@@ -2,7 +2,6 @@ import logging
 from datetime import date, timedelta
 from typing import Any, Iterable, Optional, Sequence, cast
 
-from typing_extensions import NotRequired, TypedDict
 
 import numpy.typing as npt
 
@@ -13,6 +12,16 @@ from sciety_labs.providers.article_recommendation import (
     ArticleRecommendationFilterParameters,
     ArticleRecommendationList
 )
+from sciety_labs.providers.async_providers.opensearch.typing import (
+    DocumentCrossrefAuthorDict,
+    DocumentCrossrefDict,
+    DocumentDict,
+    DocumentEuropePmcAuthorDict,
+    DocumentEuropePmcDict,
+    DocumentS2AuthorDict,
+    DocumentS2Dict,
+    DocumentScietyDict
+)
 from sciety_labs.utils.datetime import get_utcnow
 from sciety_labs.utils.distance import cosine_similarity
 
@@ -21,66 +30,6 @@ LOGGER = logging.getLogger(__name__)
 
 
 DEFAULT_OPENSEARCH_MAX_RECOMMENDATIONS = 50
-
-
-class DocumentCrossrefAuthorDict(TypedDict):
-    orcid: NotRequired[str]
-    family_name: NotRequired[str]
-    given_name: NotRequired[str]
-    sequence: NotRequired[str]
-    suffix: NotRequired[str]
-
-
-class DocumentCrossrefDict(TypedDict):
-    title_with_markup: NotRequired[str]
-    publication_date: NotRequired[str]
-    author_list: NotRequired[Sequence[DocumentCrossrefAuthorDict]]
-
-
-class DocumentS2AuthorDict(TypedDict):
-    name: str
-    s2_author_id: NotRequired[str]
-
-
-class DocumentS2Dict(TypedDict):
-    title: str
-    author_list: NotRequired[Sequence[DocumentS2AuthorDict]]
-
-
-class DocumentEuropePmcCollectiveAuthorDict(TypedDict):
-    collective_name: NotRequired[str]
-
-
-class DocumentEuropePmcIndividualAuthorDict(TypedDict):
-    full_name: NotRequired[str]
-    initials: NotRequired[str]
-    last_name: NotRequired[str]
-    first_name: NotRequired[str]
-
-
-class DocumentEuropePmcAuthorDict(
-    DocumentEuropePmcCollectiveAuthorDict,
-    DocumentEuropePmcIndividualAuthorDict
-):
-    pass
-
-
-class DocumentEuropePmcDict(TypedDict):
-    title_with_markup: NotRequired[str]
-    first_publication_date: NotRequired[str]
-    author_list: NotRequired[Sequence[DocumentEuropePmcAuthorDict]]
-
-
-class DocumentScietyDict(TypedDict):
-    evaluation_count: NotRequired[int]
-
-
-class DocumentDict(TypedDict):
-    doi: str
-    crossref: NotRequired[DocumentCrossrefDict]
-    s2: NotRequired[DocumentS2Dict]
-    europepmc: NotRequired[DocumentEuropePmcDict]
-    sciety: NotRequired[DocumentScietyDict]
 
 
 def get_author_names_for_document_s2_authors(
