f1bb269b2e558043383feb4cabfe2b7fc129de80
Daniel Ecer <de-code@users.noreply.github.com>
2024-05-15 16:47:57 +0100

    Extracted get_categories_dict_by_doi

--- sciety_labs/app/routers/api/categorisation/router.py
+++ sciety_labs/app/routers/api/categorisation/router.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Mapping, Optional
 
 import fastapi
 
@@ -7,6 +8,24 @@ from sciety_labs.utils.fastapi import get_cache_control_headers_for_request
 
 
 LOGGER = logging.getLogger(__name__)
+
+
+async def get_categories_dict_by_doi(
+    article_doi: str,
+    app_providers_and_models: AppProvidersAndModels,
+    headers: Optional[Mapping[str, str]] = None
+) -> dict:
+    doc = await (
+        app_providers_and_models
+        .async_opensearch_client
+        .get_source(
+            index=app_providers_and_models.opensearch_config.index_name,
+            id=article_doi,
+            _source_includes=['crossref.group_title'],
+            headers=headers
+        )
+    )
+    return doc
 
 
 def create_api_categorisation_router(
@@ -21,15 +40,9 @@ def create_api_categorisation_router(
         request: fastapi.Request,
         article_doi: str
     ):
-        doc = await (
-            app_providers_and_models
-            .async_opensearch_client
-            .get_source(
-                index=app_providers_and_models.opensearch_config.index_name,
-                id=article_doi,
-                _source_includes=['crossref.group_title'],
-                headers=get_cache_control_headers_for_request(request)
-            )
+        return get_categories_dict_by_doi(
+            article_doi=article_doi,
+            app_providers_and_models=app_providers_and_models,
+            headers=get_cache_control_headers_for_request(request)
         )
-        return doc
     return router
