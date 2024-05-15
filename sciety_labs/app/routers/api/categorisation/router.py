9d093f6bc75f3101d065d8a0eb875c94d0c43f82
Daniel Ecer <de-code@users.noreply.github.com>
2024-05-15 16:53:36 +0100

    Converted to class to for better abstraction of dependencies

--- sciety_labs/app/routers/api/categorisation/router.py
+++ sciety_labs/app/routers/api/categorisation/router.py
@@ -10,28 +10,34 @@ from sciety_labs.utils.fastapi import get_cache_control_headers_for_request
 LOGGER = logging.getLogger(__name__)
 
 
-async def get_categories_dict_by_doi(
-    article_doi: str,
-    app_providers_and_models: AppProvidersAndModels,
-    headers: Optional[Mapping[str, str]] = None
-) -> dict:
-    doc = await (
-        app_providers_and_models
-        .async_opensearch_client
-        .get_source(
-            index=app_providers_and_models.opensearch_config.index_name,
-            id=article_doi,
-            _source_includes=['crossref.group_title'],
-            headers=headers
+class AsyncOpenSearchCategoriesProvider:
+    def __init__(self, app_providers_and_models: AppProvidersAndModels):
+        self.async_opensearch_client = app_providers_and_models.async_opensearch_client
+        self.index_name = app_providers_and_models.opensearch_config.index_name
+
+    async def get_categories_dict_by_doi(
+        self,
+        article_doi: str,
+        headers: Optional[Mapping[str, str]] = None
+    ) -> dict:
+        doc = await (
+            self.async_opensearch_client.get_source(
+                index=self.index_name,
+                id=article_doi,
+                _source_includes=['crossref.group_title'],
+                headers=headers
+            )
         )
-    )
-    return doc
+        return doc
 
 
 def create_api_categorisation_router(
     app_providers_and_models: AppProvidersAndModels
 ) -> fastapi.APIRouter:
     router = fastapi.APIRouter()
+    async_opensearch_categories_provider = AsyncOpenSearchCategoriesProvider(
+        app_providers_and_models=app_providers_and_models
+    )
 
     @router.get(
         '/categorisation/v1/categories/by/doi'
@@ -40,9 +46,8 @@ def create_api_categorisation_router(
         request: fastapi.Request,
         article_doi: str
     ):
-        return get_categories_dict_by_doi(
+        return async_opensearch_categories_provider.get_categories_dict_by_doi(
             article_doi=article_doi,
-            app_providers_and_models=app_providers_and_models,
             headers=get_cache_control_headers_for_request(request)
         )
     return router
