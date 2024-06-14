296702d76c2db0a42192e81f8f5ec5bbe9ed4e51
Daniel Ecer <de-code@users.noreply.github.com>
2024-06-14 17:12:45 +0100

    Fixed API returns 400 for OpenSearch connection error (#463)
    * def get_default_jsonapi_error_json_response(
    Introduced
    
    * Test get_default_jsonapi_error_json_response instead
    
    * Provide correct status code for each error response

--- tests/unit_tests/app/routers/api/utils/jsonapi_test.py
+++ tests/unit_tests/app/routers/api/utils/jsonapi_test.py
@@ -1,19 +1,22 @@
+import json
 import logging
 
 import fastapi
 
-from sciety_labs.app.routers.api.utils.jsonapi import get_default_jsonapi_error_json_response_dict
+from sciety_labs.app.routers.api.utils.jsonapi import get_default_jsonapi_error_json_response
 
 
 LOGGER = logging.getLogger(__name__)
 
 
-class TestGetDefaultJsonApiErrorJsonResponseDict:
+class TestGetDefaultJsonApiErrorJsonResponse:
     def test_should_support_generic_exception(self):
         exception = AssertionError('test')
-        assert get_default_jsonapi_error_json_response_dict(
+        json_response = get_default_jsonapi_error_json_response(
             exception
-        ) == {
+        )
+        assert json_response.status_code == 500
+        assert json.loads(json_response.body) == {
             'errors': [{
                 'title': 'AssertionError',
                 'detail': 'test',
@@ -26,9 +29,11 @@ class TestGetDefaultJsonApiErrorJsonResponseDict:
             status_code=123,
             detail='Error details 1'
         )
-        assert get_default_jsonapi_error_json_response_dict(
+        json_response = get_default_jsonapi_error_json_response(
             exception
-        ) == {
+        )
+        assert json_response.status_code == 123
+        assert json.loads(json_response.body) == {
             'errors': [{
                 'title': 'HTTPException',
                 'detail': 'Error details 1',
@@ -40,11 +45,12 @@ class TestGetDefaultJsonApiErrorJsonResponseDict:
         exception = fastapi.exceptions.RequestValidationError(
             errors=[{'key': 'Error 1'}]
         )
-        json_response_dict = get_default_jsonapi_error_json_response_dict(
+        json_response = get_default_jsonapi_error_json_response(
             exception
         )
-        LOGGER.debug('json_response_dict: %r', json_response_dict)
-        assert json_response_dict == {
+        LOGGER.debug('json_response: %r', json_response)
+        assert json_response.status_code == 400
+        assert json.loads(json_response.body) == {
             'errors': [{
                 'title': 'RequestValidationError',
                 'detail': (
