1bdc497e63f7853c468d576cddd924f0cb55cab9
Daniel Ecer <de-code@users.noreply.github.com>
2024-08-05 09:21:58 +0100

    Delete async requests cache

--- tests/unit_tests/app/conftest.py
+++ tests/unit_tests/app/conftest.py
@@ -1,5 +1,9 @@
 from unittest.mock import AsyncMock, MagicMock
 
+import aiohttp_client_cache
+import aiohttp_client_cache.backends
+import aiohttp_client_cache.backends.base
+import aiohttp_client_cache.session
 import opensearchpy
 import pytest
 
@@ -10,7 +14,17 @@ from sciety_labs.providers.interfaces.async_article_recommendation import (
 
 @pytest.fixture(name='app_providers_and_models_mock')
 def _app_providers_and_models_mock() -> MagicMock:
-    return MagicMock(name='app_providers_and_models')
+    return MagicMock(name='app_providers_and_models_mock')
+
+
+@pytest.fixture(name='async_cached_client_session_mock', autouse=True)
+def _async_cached_client_session_mock(
+    app_providers_and_models_mock: MagicMock
+) -> AsyncMock:
+    mock = AsyncMock(aiohttp_client_cache.session.CachedSession)
+    mock.cache = AsyncMock(aiohttp_client_cache.backends.base.CacheBackend)
+    app_providers_and_models_mock.async_cached_client_session = mock
+    return mock
 
 
 @pytest.fixture(name='async_single_article_recommendation_provider_mock', autouse=True)
